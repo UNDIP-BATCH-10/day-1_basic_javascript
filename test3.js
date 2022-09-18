1699f3b074f7c2bb2aa89352ec7a1084fe113cd2
Satrio Purbo <satrio_leonheart@yahoo.com>
2022-09-18 22:29:55 +0700

    qweqe

--- test3.js
+++ test3.js
@@ -2,19 +2,6 @@
 Diketahui ada bilangan 1 sampai dengan 500. Tugas anda adalah mencari JUMLAH bilangan prima yang ada di antara bilangan itu
 */
 
-let  angkanya =10
-let jml=1
-for(let i =2;i<=angkanya.length;i++){
-    let x=true
-    for(let j=3;j<=angkanya.length;j++){
-        if(j%i==0){
-            x=false
-        }
-    }
-    if(x==true){
-        jml++
-    }
-    console.log(x);
-}
+
 
 // console.log(jml);
