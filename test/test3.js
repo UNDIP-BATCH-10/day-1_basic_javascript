d53c316b3f0b888c80389e2a7a7f289dcd80203a
bhagas koro <tump.store@gmail.com>
2022-09-19 10:22:23 +0700

    day 1

--- test/test3.js
+++
@@ -1,9 +0,0 @@
-/*
-Diketahui ada bilangan 1 sampai dengan 500. Tugas anda adalah mencari JUMLAH bilangan prima yang ada di antara bilangan itu
-*/
-
-function jumlahBilanganPrima(){
-
-}
-
-// console.log(jml);
