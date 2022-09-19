0efbda67f36dc1f9f07225a8ea8e34ec93b7dcad
sugikundip <sugik@live.undip.ac.id>
2022-09-19 12:09:08 +0700

    Sudah dikerjakan

--- basic/latihan/soal1.js
+++ basic/latihan/soal1.js
@@ -8,5 +8,5 @@ let third = 'Agustus';
 let fourth = 1945;
 let fifth = 'adalah Hari Kemerdekaan RI';
 
-let first + let second + let third + let fourth + let fifth;
+console.log (first + " " + second  + " " + third +" "+ fourth +" "+ fifth);
 //output : Tanggal 17 Agustus 1945 adalah Hari Kemerdekaan RI
