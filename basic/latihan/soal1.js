15255d0d4fb3fe7377a4b282035eec89f3c33755
Ajisri <ajis.riyananta@velintech.com>
2022-09-19 12:12:40 +0700

    sudah dikerjakan

--- basic/latihan/soal1.js
+++ basic/latihan/soal1.js
@@ -10,5 +10,5 @@ let fifth = "adalah Hari Kemerdekaan RI";
 
 //output : Tanggal 17 Agustus 1945 adalah Hari Kemerdekaan RI
 
-let kalimat = first + " " + second;
+let kalimat = first + " " + second + " " + third + " " + fourth + " " + fifth;
 console.log(kalimat);
