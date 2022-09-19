c6d73df0f7f394abcd394814990f593f25e30c09
danda purwidyatmoko <102501687+t4naka7@users.noreply.github.com>
2022-09-19 14:37:51 +0700

    Update soal1.js

--- basic/latihan/soal1.js
+++ basic/latihan/soal1.js
@@ -7,5 +7,8 @@ let second = 17;
 let third = 'Agustus';
 let fourth = 1945;
 let fifth = 'adalah Hari Kemerdekaan RI';
+let space = ' ';
+
+console.log(first+space+second+space+third+space+fourth+space+fifth);
 
 //output : Tanggal 17 Agustus 1945 adalah Hari Kemerdekaan RI
