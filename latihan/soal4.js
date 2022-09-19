c2733dd677fa1be73b2aecc373699dd1eda47749
Satrio Purbo <satrio_leonheart@yahoo.com>
2022-09-19 07:24:39 +0700

    asd

--- latihan/soal4.js
+++ latihan/soal4.js
@@ -1,12 +1,13 @@
-/*
-Diketahui ada bilangan 1 sampai dengan 500. Tugas anda adalah mencari bilangan prima diantara bilangan tersebut. Dan lakukan console.log pada bilangan tersebut
-*/
+// buatlah sebuah fungsi untuk mengetahui apakah suatu bilangan adalah bilangan prima
 
-function bilanganPrima(){
 
-}
+function prima(num) {
 
-//2 adalah bilangan prima
-//3 adalah bilangan prima
-//5 adalah bilangan prima
-//dan seterusnya :)
+  }
+  
+  console.log(prima(1)) //false
+  console.log(prima(2)) //true
+  console.log(prima(5)) //true
+  console.log(prima(45)) //false
+  console.log(prima(23)) //true
+  console.log(prima(230)) //false
