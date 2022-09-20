24b3d128b6b213bc29b6a8e188ea3e39bea1a13e
musacintakasih <95422165+musacintakasih@users.noreply.github.com>
2022-09-21 05:55:21 +0700

    Update soal3.js

--- basic/latihan/soal3.js
+++ basic/latihan/soal3.js
@@ -1,14 +1,24 @@
 /*
-DNA adalah molekul penyimpan informasi utama dalam sistem biologis. Ini terdiri dari empat basa asam nukleat Guanin ('G'), Sitosin ('C'), Adenin ('A'), dan Timin ('T').
+DNA adalah molekul penyimpan informasi utama dalam sistem biologis. 
+ni terdiri dari empat basa asam nukleat Guanin ('G'), Sitosin ('C'), Adenin ('A'), dan Timin ('T').
 
-RNA, adalah molekul pembawa pesan utama dalam sel. RNA sedikit berbeda dari DNA struktur kimianya dan tidak mengandung Timin. Dalam RNA Timin digantikan oleh asam nukleat lain, Urasil ('U').
+RNA, adalah molekul pembawa pesan utama dalam sel. 
+RNA sedikit berbeda dari DNA struktur kimianya dan tidak mengandung Timin. 
+Dalam RNA Timin digantikan oleh asam nukleat lain, Urasil ('U').
 */
 
 let DNA1 = "TTTT"
 let DNA2 = "GCAT"
 let DNA3 = "GACCGCCGCC"
 
+// replace T with U in DNA1
+DNA1 = DNA1.replace(/T/g, 'U')
 
+// replace T with U in DNA2
+DNA2 = DNA2.replace(/T/g, 'U')
+
+// replace T with U in DNA3
+DNA3 = DNA3.replace(/T/g, 'U')
 
 //output
 console.log(DNA1) // "UUUU"
