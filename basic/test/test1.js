393a8c5672c487ae2c1bd201e38c96789ca86c99
frschy <fariscahya1998@gmail.com>
2022-09-20 22:41:03 +0700

    sakisone

--- basic/test/test1.js
+++ basic/test/test1.js
@@ -8,7 +8,13 @@ Untuk soal nomor ini, gunakan FUNCTION yang berisi CONDITIONAL dan LOOPING untuk
 
 
 function singkatan (nama) {
-   
+   let hasil =' ';
+   let kata = nama.split(' ');
+   for (let i=0 ; i < kata.length; i++) {
+    hasil +=kata[i][0] + '.';
+   }
+   hasil = hasil.slice(0, -1);
+   return hasil;
 }
 
 console.log(singkatan('Raul Lemos')); //R.L
