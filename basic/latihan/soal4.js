96153166343c4f90543ff12d04feadaac9b8fbb9
musacintakasih <95422165+musacintakasih@users.noreply.github.com>
2022-09-21 05:55:47 +0700

    Update soal4.js

--- basic/latihan/soal4.js
+++ basic/latihan/soal4.js
@@ -1,10 +1,19 @@
 // buatlah sebuah fungsi untuk mengetahui apakah suatu bilangan adalah bilangan prima
 
-
 function prima(num) {
-
+  var x = 0;
+  for (var i = 1; i <= num; i++) {
+      if (num % i === 0) {
+          x++;
+      }
   }
-  
+  if (x === 2) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
   console.log(prima(1)) //false
   console.log(prima(2)) //true
   console.log(prima(5)) //true
