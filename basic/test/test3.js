f615f0eb9cce620ef95f2a1efb3a1fbfdb5aac86
yuyaindou <yuyaindou@gmail.com>
2022-09-22 14:59:07 +0700

    yuyaindou

--- basic/test/test3.js
+++ basic/test/test3.js
@@ -3,7 +3,22 @@ Diketahui ada bilangan 1 sampai dengan 500. Tugas anda adalah mencari JUMLAH bil
 */
 
 function jumlahBilanganPrima(){
-
+    let jumlah=0;
+    for(let a=1;a<=500;a++){
+        let pembagi = 0;
+        for(let i=1;i<=a;i++){
+            if(a%i == 0){
+                pembagi++;
+            }
+        }
+        if(pembagi==2){
+            jumlah++;
+        }
+    }
+    return jumlah;
 }
+
+var jml=jumlahBilanganPrima();
+console.log('Terdapat bilangan prima sejumlah ' + jml);
 
 // console.log(jml);
